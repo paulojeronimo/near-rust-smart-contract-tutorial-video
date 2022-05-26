4e37867d780e41650e8b8ee0316c7f2f58886b9f
Paulo Jerônimo <paulojeronimo@gmail.com>
2022-05-26 08:45:07 -0300

    Created the _trim function

--- build.sh
+++ build.sh
@@ -6,6 +6,16 @@ input_dir=media/simplescreenrecord
 build_dir=build
 trimmed_dir=$build_dir/trimmed
 
-mkdir -p $trimmed_dir
-file=01.1.mkv
-ffmpeg -i $input_dir/$file -ss 00:00:02 -to 00:03:19 -c:v copy $trimmed_dir/$file &> $trimmed_dir/$file.log
+_trim() {
+  mkdir -p $trimmed_dir
+  file=01.1.mkv
+  ffmpeg -y -i $input_dir/$file -ss 00:00:02 -to 00:03:19 -c:v copy $trimmed_dir/$file &> $trimmed_dir/$file.log
+}
+
+op=${1:-trim}
+if ! type "_$op" &> /dev/null
+then
+  echo "Operation '_$op' not found! Aborting ..."
+  exit 1
+fi
+_$op ${@:-}
